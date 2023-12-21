5d3538f0b856236283fcf38410e7f23bae042b9c
Akhil13O7 <110362224+Akhil13O7@users.noreply.github.com>
2023-12-21 14:18:16 +0530

    Add files via upload

---
+++ PracticeProject/src/main/java/com/gl/PracticeProject/Entity/Product.java
@@ -0,0 +1,27 @@
+package com.gl.PracticeProject.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+
+@Entity
+@Builder
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int pNo;
+	
+	@Column(name="productName")
+	private String pName;
+	
+	@Column(name="quantity")
+	private int qty;
+	
+	@Column(name="unitPrice")
+	private double price;
+
+}
